9698214ed1d242e54c88e5a1260f149ba75c5baf
Marcelo <113069760+K1nghthawk@users.noreply.github.com>
2022-09-18 19:34:32 -0300

    Add files via upload

---
+++ 02-variables.js
@@ -0,0 +1,17 @@
+// VARIABLES
+// Una variable es un contenedor de información
+
+var player = "Kevin De Bruyne";
+var team = "Manchester City";
+var position = "Mediocampista";
+
+
+player = "Jordan Henderson ";
+team = "Liverpool";
+
+var player_team = player + "es jugador de " + team;
+
+console.log(player, team, position);
+
+alert(player_team);
+
